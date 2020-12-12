9cf7cc0a50822cdaa0e6cbc4b6a366e1aee7ef2d
Neil Sharma <335407771@gapps.yrdsb.ca>
2020-12-11 22:43:00 -0500

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -8,16 +8,20 @@ x = input("Insert first number")
 y = input("Insert second number")
 z = input("Insert third number")
 
+temporary_var = max(x,y)
 x = min(x,y)
-y = max(x,y)
+y = temporary_var
 
+temporary_var = max(y,z)
 y = min(y,z)
-z = max(y,z)
+z = temporary_var
 
-x = min(x,y)    # A re-sort of x and y in the case where y < x
-y = max(x,y)
+temporary_var = max(x,y)
+x = min(x,y)                   # A re-sort of x and y in the case where y < x
+y = temporary_var
 
 print(x)
 print(y)
 print(z)
 
+# Because the variables are being constantly re-assigned, the temporary variable is a must
